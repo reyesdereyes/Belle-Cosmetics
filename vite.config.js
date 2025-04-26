da5072fd46275e58cf3336df90c8878f813eff9d
jesus <jesus.enrique.0z05@gmail.com>
2025-04-26 00:22:03 -0400

    Actualizar index.html y corregir rutas de Bootstrap

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Belle-Cosmetics/', // 👈 muy importante para GitHub Pages
   plugins: [react()],
 })
