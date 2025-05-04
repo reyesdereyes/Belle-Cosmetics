48bbf048445d99a82093365435d9338f0b55ccb9
jesus <jesus.enrique.0z05@gmail.com>
2025-05-04 15:13:42 -0400

    supabase V3

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import App from './App.jsx'
 import 'primereact/resources/themes/lara-light-indigo/theme.css' // theme
 import 'primereact/resources/primereact.min.css' // core css
 import 'primeicons/primeicons.css' // icons
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 
 
